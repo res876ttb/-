099b0133cb3ac213d8e1b9c9867af7c5e7002640
res876ttb <someone10851222@gmail.com>
2017-06-11 15:37:29 +0800

    fix Cang jei code error

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 par = 0.3
-dictionary_eng = {'a': '日', 'b': '月', 'c': '金', 'd': '木', 'e': '水', 'f': '火', 'g': '土', 'h': '竹', 'i': '戈', 'j': '十', 'k': '大', 'l': '中', 'm': '一', 'n': '弓', 'o': '人', 'p': '心', 'q': '手', 'r': '口', 's': '尸', 't': '廿', 'u': '山', 'v': '女', 'w': '田', 'x': '重', 'y': '卜', 'z': '難'}
+dictionary_eng = {'a': '日', 'b': '月', 'c': '金', 'd': '木', 'e': '水', 'f': '火', 'g': '土', 'h': '竹', 'i': '戈', 'j': '十', 'k': '大', 'l': '中', 'm': '一', 'n': '弓', 'o': '人', 'p': '心', 'q': '手', 'r': '口', 's': '尸', 't': '廿', 'u': '山', 'v': '女', 'w': '田', 'z': '重', 'y': '卜', 'x': '難'}
 dictionary_eng_ind = [v for v in dictionary_eng]
 correct_rate = [[[0.01, 1] for i in range(0, 26)], # correct rate, number, 
                 [0.01 for i in range(0, 26)],      # correct rate
